072df76f0598fc813999254ae5f191ae50e2a156
asins <asinsimple@gmail.com>
2018-03-08 18:34:23 +0800

    del test edit

--- .config/fish/functions/setproxy.fish
+++ .config/fish/functions/setproxy.fish
@@ -1,6 +1,5 @@
 function setproxy --description "设置代理 支持 http https git命令代理"
-  set -Ux http_proxy http://127.0.0.1:1081
-  # set -Ux http_proxy socks5://127.0.0.1:1080
+  set -Ux http_proxy socks5://127.0.0.1:1080
   set -Ux https_proxy socks5://127.0.0.1:1080
 
   # git
