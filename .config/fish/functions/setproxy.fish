7b7391cc99819131e9ddfbe9a8a75ef029547e15
asins <asinsimple@gmail.com>
2018-03-08 18:37:12 +0800

    change

--- .config/fish/functions/setproxy.fish
+++ .config/fish/functions/setproxy.fish
@@ -7,8 +7,7 @@ function setproxy --description "设置代理 支持 http https git命令代理"
   git config https.proxy 'socks5://127.0.0.1:1080'
 
   # npm
-  brew services stop polipo
-  brew services start polipo
+  brew services restart polipo
   npm config set proxy http://127.0.0.1:1081
   npm config set https-proxy http://127.0.0.1:1081
 end
