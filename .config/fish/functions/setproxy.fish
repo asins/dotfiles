a8612d26172d6ab7a1c23994de9e41b7d5a69f28
asins <wkli@alibaba-inc.com>
2019-06-19 22:07:50 +0800

    说明

--- .config/fish/functions/setproxy.fish
+++ .config/fish/functions/setproxy.fish
@@ -1,14 +1,22 @@
 function setproxy --description "设置代理 支持 http https git命令代理"
+
+# 安装polipo 在文件中添加设置`socksParentProxy`，变成如下样子：
+# file: /usr/local/Cellar/polipo/1.1.1/homebrew.mxcl.polipo.plist
+# <array>
+#   <string>/usr/local/opt/polipo/bin/polipo</string>
+#   <string>socksParentProxy=localhost:1080</string>
+# </array>
+
   brew services restart polipo
 
-  set -Ux http_proxy http://127.0.0.1:1081
-  set -Ux https_proxy http://127.0.0.1:1081
+  set -Ux http_proxy http://127.0.0.1:8123
+  set -Ux https_proxy http://127.0.0.1:8123
 
   # git
   git config http.proxy 'socks5://127.0.0.1:1080'
   git config https.proxy 'socks5://127.0.0.1:1080'
 
   # npm
-  npm config set proxy http://127.0.0.1:1081
-  npm config set https-proxy http://127.0.0.1:1081
+  npm config set proxy http://127.0.0.1:8123
+  npm config set https-proxy http://127.0.0.1:8123
 end
