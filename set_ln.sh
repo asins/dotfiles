b1abb3c226887f21c031892e6628de04e03cd448
asins <wkli@alibaba-inc.com>
2019-07-05 22:58:15 +0800

    将fisher不列入版本库中

--- set_ln.sh
+++ set_ln.sh
@@ -9,6 +9,7 @@ BASE=$(pwd)
 ln -sfv $BASE/.config/qingg.im  ~/.config/qingg.im
 
 # Git
+#rm -f ~/.gitconfig ~/.gitignore
 ln -sfv $BASE/.gitconfig ~/.gitconfig
 ln -sfv $BASE/.gitignore ~/.gitignore
 
@@ -16,4 +17,9 @@ ln -sfv $BASE/.gitignore ~/.gitignore
 ln -sfv $BASE/.config/fish/aliases.fish ~/.config/fish/aliases.fish
 ln -sfv $BASE/.config/fish/config.fish ~/.config/fish/config.fish
 ln -sfv $BASE/.config/fish/env.fish ~/.config/fish/env.fish
+
+# fish 函数
+mv ~/.config/fish/functions ~/.config/fish/functions_bck
 ln -sfv $BASE/.config/fish/functions ~/.config/fish/functions
+mv -r ~/.config/fish/functions_bck/* ~/.config/fish/functions/
+rm -rf ~/.config/fish/functions_bck
