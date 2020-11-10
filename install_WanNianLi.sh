e67a6b941b337ababbdcb4e4d5efcbf75a692b41
asins <asinsimple@alibaba-inc.com>
2020-11-10 12:47:50 +0800

    feat: 添加万年历软件的指定及备份

--- install_WanNianLi.sh
+++ install_WanNianLi.sh
@@ -17,8 +17,17 @@ if [ -d "$WanNianLi_Folder" ]; then # 真实文件存在
     mkdir -pv $WanNianLi_Back
   fi
 
-  mv -v "${WanNianLi_Folder}/festivals.js" $WanNianLi_Back
-  mv -v "${WanNianLi_Folder}/events.js" $WanNianLi_Back
+  if [ -f "${WanNianLi_Folder}/festivals.js" ]; then # 真实文件存在
+    mv -v "${WanNianLi_Folder}/festivals.js" $WanNianLi_Back
+  elif [ -L $target ]; then # 存在的是软链接
+    rm "${WanNianLi_Folder}/festivals.js"
+  fi
+
+  if [ -f "${WanNianLi_Folder}/events.js" ]; then # 真实文件存在
+    mv -v "${WanNianLi_Folder}/events.js" $WanNianLi_Back
+  elif [ -L $target ]; then # 存在的是软链接
+    rm "${WanNianLi_Folder}/events.js"
+  fi
 
   ln -sfv "${BASE_PATH}/WanNianLi/festivals.js" "${WanNianLi_Folder}/festivals.js"
   ln -sfv "${BASE_PATH}/WanNianLi/events.js" "${WanNianLi_Folder}/events.js"
