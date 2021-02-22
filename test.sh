c4f41c2740b8f1f8a3c13fac9aa0a0ff07f2abd7
asins <asinsimple@alibaba-inc.com>
2021-02-22 20:32:47 +0800

    fix: install.sh error

--- test.sh
+++ test.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-cd $(dirname $BASH_SOURCE)
-BASE_PATH=$(pwd)
+realpath() {
+    path=`eval echo "$1"`
+    folder=$(dirname "$path")
+    echo $(cd "$folder"; pwd)/$(basename "$path");
+}
 
-echo $BASE_PATH
+abs_path () {
+   echo "$(cd $(dirname "$1");pwd)/$(basename "$1")"
+}
+
+function normpath() {
+  # Remove all /./ sequences.
+  local path=${1//\/.\//\/}
+
+  # Remove dir/.. sequences.
+  while [[ $path =~ ([^/][^/]*/\.\./) ]]; do
+    path=${path/${BASH_REMATCH[0]}/}
+  done
+  echo $path
+}
+
+target=$(realpath "~/test")
+
+echo $target
+
+if [ -L "${target}" ]; then # 存在的是软链接
+  echo '软链接存在'
+elif [ -f "${target}" ]; then # 真实文件存在
+  echo '真实文件存在'
+else
+  echo '没检测出来'
+fi
